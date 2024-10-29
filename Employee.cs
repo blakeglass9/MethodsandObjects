5d512486d5b2711b83602bb151c6967840eff6c4
Blake Glass <146895621+blakeglass9@users.noreply.github.com>
2024-10-29 14:27:06 -0700

    Create Employee.cs

---
+++ Employee.cs
@@ -0,0 +1,9 @@
+namespace EmployeeApp
+{
+    // Employee class that inherits from Person
+    public class Employee : Person
+    {
+        // Property for the employee's ID
+        public int Id { get; set; }
+    }
+}
